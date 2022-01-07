74bd28e9ef2b010d5ee531d7a6f9a591f219243e
Grig Alex <alexengrigdev@gmail.com>
2022-01-07 19:06:22 +0300

    Create Customer and CustomerService

---
+++ customer/src/main/java/dev/alexengrig/sample/customer/service/CustomerService.java
@@ -0,0 +1,9 @@
+package dev.alexengrig.sample.customer.service;
+
+import dev.alexengrig.sample.customer.domain.Customer;
+
+public interface CustomerService {
+
+    void create(Customer customer);
+
+}
