ffb41a206e269c1b3252f4932ea732bc878b4015
Grig Alex <alexengrigdev@gmail.com>
2022-01-07 23:40:04 +0300

    Add fraud checking to customer

---
+++ customer/src/main/java/dev/alexengrig/sample/customer/payload/FraudCheckResponse.java
@@ -0,0 +1,16 @@
+package dev.alexengrig.sample.customer.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class FraudCheckResponse {
+
+    private boolean isFraudster;
+
+}
--- customer/src/main/java/dev/alexengrig/sample/customer/service/SimpleCustomerService.java
+++ customer/src/main/java/dev/alexengrig/sample/customer/service/SimpleCustomerService.java
@@ -4,8 +4,10 @@ import dev.alexengrig.sample.customer.CustomerRepository;
 import dev.alexengrig.sample.customer.domain.Customer;
 import dev.alexengrig.sample.customer.entity.CustomerEntity;
 import dev.alexengrig.sample.customer.mapper.CustomerMapper;
+import dev.alexengrig.sample.customer.payload.FraudCheckResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 @RequiredArgsConstructor
@@ -13,11 +15,19 @@ public class SimpleCustomerService implements CustomerService {
 
     private final CustomerRepository repository;
     private final CustomerMapper mapper;
+    private final RestTemplate restTemplate;
 
     @Override
     public void create(Customer customer) {
         CustomerEntity entity = mapper.domainToEntity(customer);
-        repository.save(entity);
+        repository.saveAndFlush(entity);
+        FraudCheckResponse checkResponse = restTemplate.getForObject(
+                "http://localhost:8081/api/v1/fraud/check/{customerId}",
+                FraudCheckResponse.class,
+                entity.getId());
+        if (checkResponse.isFraudster()) {
+            throw new IllegalArgumentException("fraudster");
+        }
     }
 
 }
