76c20d43b417bca7c95edebeb494b1d1d3c20ee2
Grig Alex <alexengrigdev@gmail.com>
2022-01-08 01:24:36 +0300

    Add fraud-client to customer

--- customer/src/main/java/dev/alexengrig/sample/customer/CustomerApplication.java
+++ customer/src/main/java/dev/alexengrig/sample/customer/CustomerApplication.java
@@ -3,9 +3,11 @@ package dev.alexengrig.sample.customer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableFeignClients(basePackages = "dev.alexengrig.sample")
 public class CustomerApplication {
 
     public static void main(String[] args) {
--- customer/src/main/java/dev/alexengrig/sample/customer/service/SimpleCustomerService.java
+++ customer/src/main/java/dev/alexengrig/sample/customer/service/SimpleCustomerService.java
@@ -1,13 +1,13 @@
 package dev.alexengrig.sample.customer.service;
 
-import dev.alexengrig.sample.customer.repository.CustomerRepository;
 import dev.alexengrig.sample.customer.domain.Customer;
 import dev.alexengrig.sample.customer.entity.CustomerEntity;
 import dev.alexengrig.sample.customer.mapper.CustomerMapper;
-import dev.alexengrig.sample.customer.payload.FraudCheckResponse;
+import dev.alexengrig.sample.customer.repository.CustomerRepository;
+import dev.alexengrig.sample.fraud.client.FraudClient;
+import dev.alexengrig.sample.fraud.payload.FraudCheckResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 @Service
 @RequiredArgsConstructor
@@ -15,16 +15,13 @@ public class SimpleCustomerService implements CustomerService {
 
     private final CustomerRepository repository;
     private final CustomerMapper mapper;
-    private final RestTemplate restTemplate;
+    private final FraudClient fraudClient;
 
     @Override
     public void create(Customer customer) {
         CustomerEntity entity = mapper.domainToEntity(customer);
         repository.saveAndFlush(entity);
-        FraudCheckResponse checkResponse = restTemplate.getForObject(
-                "http://FRAUD/api/v1/fraud/check/{customerId}",
-                FraudCheckResponse.class,
-                entity.getId());
+        FraudCheckResponse checkResponse = fraudClient.check(entity.getId());
         if (checkResponse.isFraudster()) {
             throw new IllegalArgumentException("fraudster");
         }
