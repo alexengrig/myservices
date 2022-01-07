365c5258c3e8fd4fc39a0905263bfd426d414fcb
Grig Alex <alexengrigdev@gmail.com>
2022-01-08 01:23:07 +0300

    Create fraud-payload module

--- fraud-payload/src/main/java/dev/alexengrig/sample/fraud/payload/FraudCheckResponse.java
+++ fraud-payload/src/main/java/dev/alexengrig/sample/fraud/payload/FraudCheckResponse.java
@@ -1,10 +1,14 @@
 package dev.alexengrig.sample.fraud.payload;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class FraudCheckResponse {
 
     private boolean isFraudster;
