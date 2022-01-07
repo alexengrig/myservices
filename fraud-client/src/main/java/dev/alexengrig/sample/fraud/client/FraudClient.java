d54cebff8676a7d6f97cf06d8a1dbb57619f13a9
Grig Alex <alexengrigdev@gmail.com>
2022-01-08 01:23:21 +0300

    Create fraud-client module

---
+++ fraud-client/src/main/java/dev/alexengrig/sample/fraud/client/FraudClient.java
@@ -0,0 +1,14 @@
+package dev.alexengrig.sample.fraud.client;
+
+import dev.alexengrig.sample.fraud.payload.FraudCheckResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient("fraud")
+public interface FraudClient {
+
+    @GetMapping("/api/v1/fraud/check/{customerId}")
+    FraudCheckResponse check(@PathVariable("customerId") Long customerId);
+
+}
